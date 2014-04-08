83f627047284e4cfa1015d71f3480b9605b08f1a
Zvi Schutz <zvi@crossrider.com>
2014-04-08 13:17:35 +0300

    trygit #3 - Take V

--- extension.js
+++ extension.js
@@ -8,6 +8,6 @@ appAPI.ready(function($) {
     // Place your code here (you can also define new functions above this scope)
     // The $ object is the extension's jQuery object
 
-    alert("My github 3 take IV - extension works! The current page is: " + document.location.href);
+    alert("My github 3 take V - extension works! The current page is: " + document.location.href);
 
 });
