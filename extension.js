06915897efd1c5b5e225c864c82d7d488867b688
Zvi Schutz <zvi@crossrider.com>
2014-04-08 12:44:28 +0300

    trygit #3 - Take I

--- extension.js
+++ extension.js
@@ -8,6 +8,6 @@ appAPI.ready(function($) {
     // Place your code here (you can also define new functions above this scope)
     // The $ object is the extension's jQuery object
 
-    alert("My new Crossrider extension works! The current page is: " + document.location.href);
+    alert("My github 3 extension works! The current page is: " + document.location.href);
 
 });
